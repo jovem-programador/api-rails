1fd79678a1d6b200f47afb79438300acacbdcba1
Anderson Marley <andersonmarley011@gmail.com>
2024-01-29 22:59:37 -0300

    Colocando dados

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Task.create title: 'A (server)', done: false
+Task.create title: 'B (server)', done: true
+Task.create title: 'C (server)', done: false
+Task.create title: 'D (server)', done: true
